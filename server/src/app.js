d5317b1dbfde236efd5a567664efca247c6faaed
Longfei Ma <mg071121mg@gmail.com>
2019-02-12 18:32:47 -0500

    makeSomeChanges

--- server/src/app.js
+++ server/src/app.js
@@ -9,10 +9,10 @@ app.use(morgan('combined'))
 app.use(bodyparser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
   res.send({
-    message: 'hello world!'
+    message: `Hello ${req.body.email}! Your user was registered! Have fun!`
   })
 })
 
-app.listen(process.env.PORT || 8082)
+app.listen(process.env.PORT || 8080)
